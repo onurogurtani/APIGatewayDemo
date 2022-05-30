d07b2ce3ca44f6d4318b7f07517425d616700ce6
Onur Oğurtanı <onur.ogurtani@kariyer.net>
2022-05-30 13:04:00 +0300

    update

--- APIGatewayConfigurationWithConsulDemo/APIGateway/Modules/SwaggerModule.cs
+++ APIGatewayConfigurationWithConsulDemo/APIGateway/Modules/SwaggerModule.cs
@@ -1,5 +1,4 @@
-﻿using APIGateway.Settings;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
 using System.Reflection;
